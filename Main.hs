d30b5186461fc3034315f50465cf66bf5fa112be
davnils <nilsson.dd+github+gravatarobscurity@gmail.com>
2013-03-16 16:34:34 +0100

    Some improvements. PHPSESSID seen as temporary token which signs and builds an authenticated request.

--- Main.hs
+++ Main.hs
@@ -22,41 +22,34 @@ loginSuccess = "Login successful"
 type ConnEnv m = ReaderT Connection m
 type AuthEnv m = ReaderT B.ByteString (ConnEnv m)
 
-signRequest :: AuthEnv IO (RequestBuilder ())
-signRequest = ask >>= return . setHeader "Cookie"
+makeSignedRequest :: RequestBuilder () -> AuthEnv IO Request
+makeSignedRequest req = do
+  key <- liftM (setHeader "Cookie") ask
+  liftIO . buildRequest $ req >> key
 
 defaultRequest :: RequestBuilder ()
 defaultRequest = do
   setHeader "User-Agent" "Sofie"
   setHeader "Connection" "keep-alive"
 
--- confirmed working with using retrievePage on a predefined cookie (with only sessid) from token
-
 -- send some files to the upload page, retrieve submission id
 submitSolution :: AuthEnv IO ()
 submitSolution = return ()
 
-retrievePage :: B.ByteString -> AuthEnv IO ()
+retrievePage :: B.ByteString -> AuthEnv IO B.ByteString
 retrievePage page = do
-  conn <- lift ask
-  sign <- signRequest
-  header <- liftIO $ buildRequest $ do
+  header <- makeSignedRequest $ do
     http GET page
     defaultRequest
-    sign
 
-  liftIO $ putStrLn "-----------------"
-  liftIO $ putStrLn $ "Sending header: " ++ show header
-
+  conn <- lift ask
   liftIO $ sendRequest conn header emptyBody
-  liftIO $ receiveResponse conn debugHandler
-
--- TODO: Does the client save the correct cookie? the last one should be saved.
+  liftIO $ receiveResponse conn concatHandler
 
 authenticate :: ConnEnv IO (Maybe B.ByteString)
 authenticate = do
   conn <- ask
-  header <- lift $ buildRequest $ do
+  header <- liftIO . buildRequest $ do
     http POST loginPage
     defaultRequest
     setContentType "application/x-www-form-urlencoded"
@@ -64,26 +57,25 @@ authenticate = do
   let formData = [("token", token), ("user", user), ("script", "true")] 
   liftIO $ sendRequest conn header $ encodedFormBody formData
 
-  -- parse response header and verify that body content matches the magic string
+  -- TODO: Catch exception upon failed read
   result <- liftIO $ receiveResponse conn (\headers stream -> do
     response <- readExactly (B.length loginSuccess) stream
-    putStrLn $ "Received response: '" ++ B.unpack response ++ "' and headers: " ++ show headers
     return (headers, response == loginSuccess))
 
-  case result of
-    (_, False) -> return Nothing
-    (headers, _) -> return $
+  return $ case result of
+    (_, False) -> Nothing
+    (headers, _) ->  
       (B.words <$> getHeader headers "Set-Cookie") >>=
       find ("PHPSESSID" `B.isPrefixOf`) >>=
       return . B.takeWhile (/= ';')
 
 main :: IO ()
-main = withOpenSSL $ withConnection (establishConnection host) $ runReaderT work
+main = withOpenSSL . withConnection (establishConnection host) $ runReaderT go
   where
-  work = do
+  go = do
     status <- authenticate
     case status of
       Nothing -> liftIO $ putStrLn "Failed to authenticate"
       Just session -> do
-        liftIO $ putStrLn $ "authenticate returned cookie: '" ++ B.unpack session ++ "'"
-        runReaderT (submitSolution >> retrievePage "/judge_upload") session
+        liftIO . putStrLn $ "authenticate returned temporary token: '" ++ B.unpack session ++ "'"
+        runReaderT (submitSolution >> retrievePage "/judge_upload" >>= liftIO . putStrLn . B.unpack) session
